5c4bf74887fbfd6d74272047408836d22643b0bc
Mark Stillwell <mark@stillwell.me>
2014-08-10 00:04:54 -0400

    tinkering with bootstrap...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+GIT=$(which git)
+if [ -z "${GIT}" ]; then
+  echo "git required."
+  exit 1
+fi
+
+VCSH=$(which vcsh)
+if [ -z "${VCSH}" ]; then
+  VCSH="${HOME}/.local/bin/vcsh"
+  curl https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh -o "${VCSH}"
+fi
+
+MR=$(which mr)
+if [ -z "${MR}" ]; then
+  MR="${HOME}/.local/bin/mr"
+  curl https://raw.githubusercontent.com/RichiH/vcsh/master/vcsh -o "${VCSH}"
+fi
+
 #: ${VCSH_HOOK_D:="$XDG_CONFIG_HOME/vcsh/hooks-enabled"}
 export VCSH_HOOK_D="${HOME}/.config/vcsh/hooks-enabled"
 mkdir -p "${VCSH_HOOK_D}"
@@ -9,9 +27,7 @@ cd "${VCSH_HOOK_D}"
 HOOK_SCRIPT="post-init.sparse-checkout"
 cat > "${HOOK_SCRIPT}" << HOOK
 #!/bin/sh
-if ! test "\$(git config core.sparseCheckout)" = "true"; then
-    git config core.sparseCheckout true
-fi
+git config core.sparseCheckout true
 cat >> \$GIT_DIR/info/sparse-checkout << EOF
 *
 !README*
@@ -22,7 +38,6 @@ cat >> \$GIT_DIR/info/sparse-checkout << EOF
 !travis
 EOF
 HOOK
-
 chmod 755 "${HOOK_SCRIPT}"
 
-vcsh clone https://github.com/marklee77/vcsh-homedir-test.git test
+${VCSH} clone https://github.com/marklee77/vcsh-homedir-test.git test
